5ef358451a008615771b1d2e421e86b7d57bb3e1
Peter Lam <Piitaa1234@gmail.com>
2020-05-12 16:57:55 -0400

    added upload files

--- public/js/upload.js
+++ public/js/upload.js
@@ -1,24 +1,3 @@
 $(document).ready(function() {
-  var options = {
-    // pre-submit callback
-    success: showResponse,
-    // post-submit callback
-    beforeSubmit: showRequest,
-  };
-
-  // bind to the form's submit event
-  $("#frmUploader").submit(function() {
-    $(this).ajaxSubmit(options);
-    // always return false to prevent standard browser submit and page navigation
-    return false;
-  });
+  var nameOfFood = $();
 });
-// pre-submit callback
-function showRequest(formData, jqForm, options) {
-  alert("Uploading is starting.");
-  return true;
-}
-// post-submit callback
-function showResponse(responseText, statusText, xhr, $form) {
-  alert("status: " + statusText + "\n\nresponseText: \n" + responseText);
-}
