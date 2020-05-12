f0a3ecadb57304a4f140d927c5884ce8bc481934
Jiyoon Kim <jiyoon9886@gmail.com>
2020-05-12 01:47:39 -0400

    updated database name, added User table columns, updated signup html and signup.js

--- config/passport.js
+++ config/passport.js
@@ -3,9 +3,8 @@ var LocalStrategy = require("passport-local").Strategy;
 
 var db = require("../models");
 
-// Telling passport we want to use a Local Strategy. In other words, we want login with a username/email and password
+// Telling passport we want to use a Local Strategy (login with a username/email and password)
 passport.use(new LocalStrategy(
-  // Our user will sign in using an email, rather than a "username"
   {
     usernameField: "email"
   },
