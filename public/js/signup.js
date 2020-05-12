f2a3e2ec3ec2d0109e3bcf412427e946db224eaa
Jiyoon Kim <jiyoon9886@gmail.com>
2020-05-12 16:54:03 -0400

    created .env variable for super secret password

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   // Getting references to our form and input
   var signUpForm = $("form.signup");
-  var nameInput = $("input#name-input");
+  //var nameInput = $("input#name-input");
   var emailInput = $("input#email-input");
   var passwordInput = $("input#password-input");
 
@@ -9,26 +9,25 @@ $(document).ready(function() {
   signUpForm.on("submit", function(event) {
     event.preventDefault();
     var userData = {
-      name: nameInput.val().trim(), 
+      //name: nameInput.val().trim(), 
       email: emailInput.val().trim(),
       password: passwordInput.val().trim()
     };
 
-    if (!userData.name || !userData.email || !userData.password) {
+    if (!userData.email || !userData.password) {
       return;
     }
     // If we have a name, email, and password, run the signUpUser function
-    signUpUser(userData.name, userData.email, userData.password);
-    nameInput.val("");
+    signUpUser(userData.email, userData.password);
+    //nameInput.val("");
     emailInput.val("");
     passwordInput.val("");
   });
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
-  function signUpUser(name, email, password) {
+  function signUpUser(email, password) {
     $.post("/api/signup", {
-      name: name,
       email: email,
       password: password
     })
