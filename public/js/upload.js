a0bc28351ea9e9bc6d9622d29aac837242fd096f
Peter Lam <Piitaa1234@gmail.com>
2020-05-13 14:08:52 -0400

    worked on upload

--- public/js/upload.js
+++ public/js/upload.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
   // Reference to our form and inputs
+  var uploadForm = $("form.foodUpload");
   var nameOfFood = $("#foodName");
   var foodNotes = $("#foodNotes");
+
+  // Submit button event
+  uploadForm.on("submit", function(event) {
+    event.preventDefault();
+    console.log("I am submitting stuffs");
+  });
 });
